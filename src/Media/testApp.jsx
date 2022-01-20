a8d20d52aa475ddaa5ed619d6a21860ac1423ff1
Clinto_92_Abraham <clinto92@gmail.com>
2022-01-20 20:48:44 +0530

    second commit on test app

--- src/Media/testApp.jsx
+++ src/Media/testApp.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const testApp = () => {
-  return <div>test app</div>;
+  return <div>test app
+    <h1>test app commited</h1>
+  </div>;
 };
 
 export default testApp;
