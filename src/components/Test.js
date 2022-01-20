0515d2386c2354c8b8a507565cf625b3c30013a5
Clinto_92_Abraham <clinto92@gmail.com>
2022-01-20 18:59:28 +0530

    secondary commit

--- src/components/Test.js
+++ src/components/Test.js
@@ -5,6 +5,9 @@ const Test = () => {
       <h1>We are amending changes into pull request.</h1>
       <small>rebasing now</small>
       <h6>stuff will be committed to branch rebase-test</h6>
+      <h1>We are amending changes into pull request.</h1>
+      <small>rebasing now</small>
+      <h6>stuff will be committed to branch rebase-test</h6>
   </div>;
 };
 
