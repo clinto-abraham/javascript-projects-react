736466844bdf860c871f31184abc4d935172e6ed
Clinto_92_Abraham <clinto92@gmail.com>
2022-01-20 19:43:40 +0530

    tertiary commit on test2 branch

--- src/components/test3.js
+++ src/components/test3.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const test3 = () => {
   return <div>We testing simultaneously development on different branches
-      
+      <h1>we are deploying tertiary test</h1>
   </div>;
 };
 
