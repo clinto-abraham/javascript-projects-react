d5b5f49e5b82a7d5ccfadb5fc7b9b5a3b64b9568
Clinto_92_Abraham <clinto92@gmail.com>
2022-01-20 16:10:03 +0530

    test 1 commit - added with test 2 commit

---
+++ src/components/Test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Test = () => {
+  return <div>We are ammending
+      <H1>We are amending changes into pull request.</H1>
+      <small>rebasing now</small>
+  </div>;
+};
+
+export default Test;
