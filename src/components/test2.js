577fca5d01b39d5707e9364b6fd70cbd02add488
Clinto_92_Abraham <clinto92@gmail.com>
2022-01-20 19:34:46 +0530

    test2 is pushed first commit

---
+++ src/components/test2.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const test2 = () => {
+  return <div>Secondary rebase test</div>;
+};
+
+export default test2;
---
+++ src/components/test3.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const test3 = () => {
+  return <div>We testing simultaneously development on different branches</div>;
+};
+
+export default test3;
