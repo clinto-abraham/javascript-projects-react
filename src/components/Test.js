ae270c98fb1b7e88aed276ea53e3ea02703f38c4
Clinto_92_Abraham <clinto92@gmail.com>
2022-01-20 19:24:46 +0530

    changes to be pushed on pull request

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Test = () => {
-<<<<<<< HEAD
+
   return <div>We are ammending
       <h1>We are amending changes into pull request.</h1>
       <small>rebasing now</small>
@@ -9,10 +9,9 @@ const Test = () => {
       <h1>We are amending changes into pull request.</h1>
       <small>rebasing now</small>
       <h6>stuff will be committed to branch rebase-test</h6>
+      <span>We are pushing changes to the pull request</span>
   </div>;
-=======
-  return <div>We are ammending</div>;
->>>>>>> d5b5f49e5b82a7d5ccfadb5fc7b9b5a3b64b9568
+
 };
 
 export default Test;
