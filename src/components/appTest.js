1d9b2fbeffe1679470aadeef95c6bf387f396609
Clinto_92_Abraham <clinto92@gmail.com>
2022-01-20 21:41:35 +0530

    Delete (#7)
    * 2
    
    * deleting unwanted test files
    
    * added todos
    
    * deleted todos

--- src/components/appTest.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const appTest = () => {
-  return <div>Testing app</div>;
-};
-
-export default appTest;
--- src/components/test2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const test2 = () => {
-  return <div>Secondary rebase test
-      <h1>we are deploying secondary test</h1>
-  </div>;
-};
-
-export default test2;
