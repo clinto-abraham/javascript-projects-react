2e8f582877aa64ce55eae03bf496d13cd647fffc
Clinto_92_Abraham <clinto92@gmail.com>
2022-01-20 20:55:54 +0530

    deleted another file

--- src/components/test3.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const test3 = () => {
-  return <div>We testing simultaneously development on different branches
-      <h1>we are deploying tertiary test</h1>
-  </div>;
-};
-
-export default test3;
