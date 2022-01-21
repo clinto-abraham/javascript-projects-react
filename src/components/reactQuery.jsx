f45750d4472c6610bb899b947f03c8805e4ea5fb
Clinto_92_Abraham <clinto92@gmail.com>
2022-01-21 11:13:52 +0530

    deleted comments

--- src/components/reactQuery.jsx
+++ src/components/reactQuery.jsx
@@ -42,12 +42,7 @@ const API = async ()=> {
         </ul>
   
         <button
-        //   onClick={() => {
-        //     mutation.mutate({
-        //       id: Date.now(),
-        //       title: 'Do Laundry',
-        //     })
-        //   }}
+        
         >
           Add Todo
         </button>
